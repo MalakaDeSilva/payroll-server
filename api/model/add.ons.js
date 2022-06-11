d1eb712a7c79c85e13b349f9b35f03328c97cf8c
Malaka Silva <malakasilva43@gmail.com>
2022-06-11 19:48:33 +0530

    add from pay cycle

--- api/model/add.ons.js
+++ api/model/add.ons.js
@@ -14,6 +14,10 @@ const addOnsSchema = mongoose.Schema(
     fixedAllowance: {
       type: Number,
     },
+    fromPayCycle: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: true }
 );
